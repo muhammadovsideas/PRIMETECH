c2833d8ae407b8f68f6e41865c919dc7162f0015
Muzaffar Muhammadov <muhammadovs.ideas@gmail.com>
2025-09-07 21:08:00 +0500

    static o'zgarishi

--- core/settings.py
+++ core/settings.py
@@ -177,13 +177,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-import os
 
-STATIC_ROOT = '/var/www/PRIMETECH/static/'
-MEDIA_ROOT = '/var/www/PRIMETECH/media/'
+STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
